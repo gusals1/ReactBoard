e64cebd15fb3f410dfd555b9ed9f1ce2fd278fda
qkrgus3318@gmail.com <qkrgus3318@gmail.com>
2024-08-07 16:22:21 +0900

    banner width 변경

--- src/components/commons/layout/banner/LayoutBanner.styles.ts
+++ src/components/commons/layout/banner/LayoutBanner.styles.ts
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
 
 export const BannerWrapper = styled.div`
-  width: 1200px;
+  display: block;
+  max-width: 1200px;
   margin: auto;
-  height: 440px;
+  height: auto;
 `;
 
 export const Banner = styled.div``;
